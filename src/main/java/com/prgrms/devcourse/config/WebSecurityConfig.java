8cfcb2ba8c0cfa874308218742e6a5da17a82191
midasworld <midas117@knou.ac.kr>
2022-05-15 19:13:30 +0900

    feat: WebSecurityConfig 생성 - 시큐리티 기본 설정 정보 추가

---
+++ src/main/java/com/prgrms/devcourse/config/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.prgrms.devcourse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/assets/**");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/me").hasAnyRole("USER", "ADMIN")
+				.anyRequest().permitAll()
+				.and()
+			.formLogin()
+				.defaultSuccessUrl("/")
+				.permitAll()
+				.and()
+		;
+
+	}
+}

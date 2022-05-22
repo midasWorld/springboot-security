32b15ab9d77b11c53030f3f4defa7e99028f80ec
midasworld <midas117@knou.ac.kr>
2022-05-22 15:45:52 +0900

    feat: 인증 이벤트 - 동기적 이벤트의 단점, Async로 해결하는 부분 실습

--- src/main/java/com/prgrms/devcourse/config/AuthenticationEvents.java
+++ src/main/java/com/prgrms/devcourse/config/AuthenticationEvents.java
@@ -1,6 +1,7 @@
 package com.prgrms.devcourse.config;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.core.Authentication;
@@ -12,8 +13,14 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class AuthenticationEvents {
 
+	@Async
 	@EventListener
 	public void handleAuthenticationSuccessEvent(AuthenticationSuccessEvent event) {
+		try {
+			Thread.sleep(5000L);
+		} catch (InterruptedException e) {
+		}
+
 		Authentication authentication = event.getAuthentication();
 		log.info("Successful authentication result: {}", authentication.getPrincipal());
 	}
